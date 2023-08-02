4497afaea784b383d84f31d340cf7d8db24a4915
Winters <songyang@iqidao.com>
2023-08-02 16:36:15 +0800

    1

--- src/app/[lang]/page.jsx
+++
@@ -1,6 +0,0 @@
-import { getDictionary } from './dictionaries'
-
-export default async function Page({ params: { lang } }) {
-  const dict = await getDictionary(lang) // en
-  return <button>{dict.products.cart}</button> // Add to Cart
-}
